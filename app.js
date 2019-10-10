dfece999529e4f4bba89e4e6a5765a01633e88e1
Jake M <scudsterr@gmail.com>
2019-10-10 14:52:35 -0400

    CSS styling and Error Middleware
    CSS adjusted and error handling added

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // Importing and setting up express
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Importing json file and defining projects variable
 const data = require('./data');
 const projects = data.projects;
 
 // Setting up static route to public folder
-app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 // Set view engine to pug
 app.set('view engine', 'pug');
@@ -35,6 +36,21 @@ app.get('/project/:id' , (req, res, next) => {
         image_square: res.locals[req.params.id].image_square,
         image_landscape: res.locals[req.params.id].image_landscape
     });
+});
+
+// 404 page not found
+app.use((req, res, next) => {
+    const err = new Error("Sorry, we couldn't find the page you were looking for.");
+    console.log("Page not found");
+    err.status = 404;
+    next(err);
+})
+
+//Error handler
+app.use((err, req, res, next) => {
+    res.locals.error = err
+    res.status(err.status)
+    res.render('error');
 });
 
 

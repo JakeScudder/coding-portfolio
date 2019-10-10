89c239b5215e6e3ce475b52da1ee9c5e420cd7b1
Jake M <scudsterr@gmail.com>
2019-10-10 16:15:01 -0400

    Trying to stop animations
    Want animations only on initial page load

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,8 @@
 $(document).foundation()
+
+
+// $("document").ready(function() {
+//         $("h1").removeClass("animated fadeInDown");
+//         $("#square-grid").removeClass("grid-container portfolio-index animated zoomIn");
+//         $("#square-grid").addClass("grid-container portfolio-index");
+//     });

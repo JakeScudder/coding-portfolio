082143255b18470f4a54d844f7326115bd2e05d4
Jake M <scudsterr@gmail.com>
2019-10-11 13:50:43 -0400

    Error Handling
    Reduction of variables in index.pug

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.set('view engine', 'pug');
 // Setting the index route
 app.get('/', (req, res, next) => {
     res.locals = projects;
-    res.render('index');
+    res.render('index', {
+        all_projects: res.locals
+    });
 });
 
 // Setting the about route
@@ -27,15 +29,22 @@ app.get('/about', (req, res, next) => {
 // Setting the dynamic projects route
 app.get('/project/:id' , (req, res, next) => {
     res.locals = projects;
-    res.render('project', {
-        title: res.locals[req.params.id].project_name,
-        description: res.locals[req.params.id].description,
-        techArray: res.locals[req.params.id].technologies,
-        live_link: res.locals[req.params.id].live_link,
-        github_link: res.locals[req.params.id].github_link,
-        image_square: res.locals[req.params.id].image_square,
-        image_landscape: res.locals[req.params.id].image_landscape
-    });
+    if (res.locals[req.params.id]) {
+        res.render('project', {
+            title: res.locals[req.params.id].project_name,
+            description: res.locals[req.params.id].description,
+            techArray: res.locals[req.params.id].technologies,
+            live_link: res.locals[req.params.id].live_link,
+            github_link: res.locals[req.params.id].github_link,
+            image_square: res.locals[req.params.id].image_square,
+            image_landscape: res.locals[req.params.id].image_landscape
+        });
+    } else {
+        console.log('else')
+        const err = new Error("Sorry, we couldn't find the project you were looking for.");
+        err.status = 404;
+        next(err);
+    }
 });
 
 // 404 page not found
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,12 @@
 $(document).foundation()
 
+setTimeout(removeClass, 5000);
 
-// $("document").ready(function() {
-//         $("h1").removeClass("animated fadeInDown");
-//         $("#square-grid").removeClass("grid-container portfolio-index animated zoomIn");
-//         $("#square-grid").addClass("grid-container portfolio-index");
-//     });
+function removeClass() {
+    let h1 = document.querySelector('h1');
+    console.log(h1);
+    h1.className="";
+    let squareGrid = document.querySelector('#square-grid');
+    squareGrid.className="grid-container portfolio-index";
+
+}

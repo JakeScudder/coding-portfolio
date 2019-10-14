01b87bffb283bb48b05e70cd746b7db2305765ab
Jake M <scudsterr@gmail.com>
2019-10-14 15:53:13 -0400

    Final touches
    Comments removed and descriptions of projects finalized for clarity.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,11 +2,10 @@ $(document).foundation()
 
 setTimeout(removeClass, 5000);
 
-function removeClass() {
-    let h1 = document.querySelector('h1');
-    console.log(h1);
-    h1.className="";
-    let squareGrid = document.querySelector('#square-grid');
-    squareGrid.className="grid-container portfolio-index";
-
-}
+// function removeClass() {
+//     let h1 = document.querySelector('h1');
+//     console.log(h1);
+//     h1.className="";
+//     let squareGrid = document.querySelector('#square-grid');
+//     squareGrid.className="grid-container portfolio-index";
+// }
